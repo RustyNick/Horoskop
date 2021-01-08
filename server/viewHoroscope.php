d9d48c2bbfa4d240d3e505970acb82d2692796b3
rusty-nick <YG91putty>
2021-01-08 20:09:22 +0100

    Redo of code
    redo of code

--- server/viewHoroscope.php
+++ server/viewHoroscope.php
@@ -2,15 +2,29 @@
 
 session_start();
 
+            if(isset($_SERVER["REQUEST_METHOD"])) {
 
-            //Check if horoscope exist in session
-            if(isset($_SESSION["horoscope"])) {
-                echo json_encode($_SESSION["horoscope"]);
-                exit;
-            } else{
-                //eventuellt lägg ett error här istället
+                if($_SERVER["REQUEST_METHOD"] === "GET"){
+
+                    if(isset($_SESSION["horoscope"])) {
+
+                        $sign = unserialize($_SESSION["horoscope"]);
+                        echo json_encode($sign);
+                        exit;
+
+
+                } else{
+                    //eventuellt lägg ett error här istället
+                    echo json_encode(false);
+                    exit;
+                    }
+
+                }else{
+                    echo json_encode(false);
+                     exit;
+                    }
+            }else{
                 echo json_encode(false);
-                exit;
-            }
-
+                 exit;
+                }
 ?>
